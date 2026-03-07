3f99bf855085a392292190815617dac0a3484148
João Vitor Goulart Viana <54647056+joaovitor-viana@users.noreply.github.com>
2026-03-06 23:02:44 -0300

    Features: Register, ForgotPassword, UI Cleanup, Auto-create Player, Year 2026

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { AuthProvider } from './context/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
 import PlayerView from './pages/PlayerView'
 import GMView from './pages/GMView'
 
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route 
             path="/player" 
             element={
